84955901b65510efefdeed5679d09a443869450a
0xfff <0xfff@protonmail.com>
2022-06-05 13:05:21 +0200

    chore: export transactionstore and hooks

--- packages/rainbowkit/src/index.ts
+++ packages/rainbowkit/src/index.ts
@@ -3,6 +3,12 @@ export { wallet } from './wallets/walletConnectors';
 export { getDefaultWallets } from './wallets/getDefaultWallets';
 export { connectorsForWallets } from './wallets/connectorsForWallets';
 export { useAddRecentTransaction } from './transactions/useAddRecentTransaction';
+export { useClearRecentTransactions } from './transactions/useClearRecentTransactions';
+export {
+  useTransactionStore,
+  TransactionStoreProvider,
+} from './transactions/TransactionStoreContext';
+export { useRecentTransactions } from './transactions/useRecentTransactions';
 export type { Wallet, WalletList } from './wallets/Wallet';
 export type { Chain } from './components/RainbowKitProvider/RainbowKitChainContext';
 export type { Theme } from './components/RainbowKitProvider/RainbowKitProvider';
